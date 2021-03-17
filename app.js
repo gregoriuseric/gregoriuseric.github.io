22ccb32b2298e597b7708913debf07841d59bf96
gregoriuseric <gregorius.eric92@gmail.com>
2021-03-17 14:57:40 +0700

    tambah animasi dan icon

--- app.js
+++ app.js
@@ -25,4 +25,90 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
+
+  // GSAP
+  gsap.from(".home-animate", {
+    y: 100,
+    duration: 1.2,
+    opacity: 0,
+    stagger: 0.3,
+  });
+
+  gsap.from(".img-home", {
+    x: 100,
+    duration: 1.2,
+    opacity: 0,
+  });
+
+  gsap.from(".about-animate", {
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top 100%",
+    },
+    y: 100,
+    duration: 1.2,
+    opacity: 0,
+    stagger: 0.3,
+  });
+
+  gsap.from(".img-about", {
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top 100%",
+    },
+    x: -100,
+    duration: 1.2,
+    opacity: 0,
+  });
+
+  gsap.from(".skill-header-animate", {
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "top 80%",
+    },
+    y: -100,
+    duration: 1.2,
+    opacity: 0,
+  });
+
+  gsap.from(".skill-item", {
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "top 80%",
+    },
+    y: 100,
+    duration: 1.2,
+    opacity: 0,
+    stagger: 0.3,
+  });
+
+  gsap.from(".contact-header", {
+    scrollTrigger: {
+      trigger: "#contact",
+      start: "top 80%",
+    },
+    y: -100,
+    duration: 1.2,
+    opacity: 0,
+  });
+
+  gsap.from(".contact-info", {
+    scrollTrigger: {
+      trigger: "#contact",
+      start: "top 80%",
+    },
+    x: -100,
+    duration: 1.2,
+    opacity: 0,
+  });
+
+  gsap.from(".contact-form", {
+    scrollTrigger: {
+      trigger: "#contact",
+      start: "top 80%",
+    },
+    x: 100,
+    duration: 1.2,
+    opacity: 0,
+  });
 });
